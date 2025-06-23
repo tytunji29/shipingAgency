dc247f12bffa09147e5d85ef75f17bb27c01fc32
tunji ty <tytunji29@gmail.com>
2025-06-23 08:57:04 +0100

    added public file

--- BackEnd/Utilities/obj/Debug/net8.0/UtilitiesServices.AssemblyInfo.cs
+++ BackEnd/Utilities/obj/Debug/net8.0/UtilitiesServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UtilitiesServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+458c6b01dd56ef02ac5199b14506af338e4a6e70")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f835f7fa9890821e847576d936106bf43e00aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("UtilitiesServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UtilitiesServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BackEnd/Utilities/obj/Release/net8.0/UtilitiesServices.AssemblyInfo.cs
+++ BackEnd/Utilities/obj/Release/net8.0/UtilitiesServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UtilitiesServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec549d71d46815c5150bf7b6cc6a0d683a3a1677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81f835f7fa9890821e847576d936106bf43e00aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("UtilitiesServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UtilitiesServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
